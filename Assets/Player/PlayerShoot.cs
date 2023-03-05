6f3b02c0958f397536436471e0f6f4da5561120c
Mitchell Opitz <mitchell@mitchellopitz.net>
2023-03-04 23:30:56 -0600

    fix - Player shoot animation

--- Assets/Player/PlayerShoot.cs
+++ Assets/Player/PlayerShoot.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections;
 
 public class PlayerShoot : MonoBehaviour
 {
@@ -9,15 +10,22 @@ public class PlayerShoot : MonoBehaviour
     public int damage = 5;
     public int upgradeRank = 0;
 
-    public Animator animator;
+    private Animator animator;
 
     private float nextFireTime = 0f;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
 
     void Update()
     {
         if (Input.GetButton("Fire1") && Time.time > nextFireTime)
         {
             Fire(damage);
+            animator.SetBool("isShooting", true);
+            StartCoroutine(AdjustParameterAfterFrames(12, "isShooting", false));
         }
 
         // Aim the gun towards the mouse cursor
@@ -25,6 +33,15 @@ public class PlayerShoot : MonoBehaviour
         Vector2 lookDir = mousePos - transform.position;
         float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg;
         gunTip.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+    IEnumerator AdjustParameterAfterFrames(int framesToWait, string parameterName, bool value)
+    {
+        for (int i = 0; i < framesToWait; i++)
+        {
+            yield return null;
+        }
+
+        animator.SetBool(parameterName, value);
     }
 
     void Fire(int damageAmount)
@@ -43,7 +60,7 @@ public class PlayerShoot : MonoBehaviour
 
             Rigidbody2D projectileRigidbody = projectile.GetComponent<Rigidbody2D>();
             projectileRigidbody.velocity = projectile.transform.right * projectileSpeed;
-        }
+        }        
     }
 }
 
